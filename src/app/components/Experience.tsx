1500c9ac92c3a42dfa848b034418329362859a48
ShreeramNew <Shreerambca1@gmail.com>
2025-08-31 14:15:52 +0530

    working on skills section

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -4,141 +4,152 @@ import { Timeline, ConfigProvider } from "antd";
 import { GoDotFill } from "react-icons/go";
 
 export default function Experience() {
-   const items = [
-      {
-         dot: <div className=" w-[15px] h-[15px] rounded-full bg-gray-200 flex-shrink-0" />,
-         children: (
-            <div className=" md:pr-[1rem]">
-               <div className=" flex justify-between items-center">
-                  <div className=" text-themeOrange text-[1.1rem]">
-                     Frontend Developer Intern{" "}
-                     <span className=" text-[0.9rem] text-gray-300">
-                        @Tikanga pvt ltd, Mangalore
-                     </span>
-                  </div>
-                  <div className=" text-gray-300 mt-1 md:mt-0 text-[0.7rem] md:text-[1rem]">Aug 2024 - Nov 2024</div>
-               </div>
-               <div className=" flex flex-col gap-[10px] mt-3">
-                  <CustomPoint>
-                     <div>
-                        Developed multiple web applications using{" "}
-                        <Highlight text={"Next.js, TypeScript, React.js"} /> and{" "}
-                        <Highlight text={"Ant Design"} />
-                     </div>
-                  </CustomPoint>
-                  <CustomPoint>
-                     <div>
-                        Worked on a payment-related web application with 2000+ users, integrating
-                        APIs, optimizing performance, and improving overall website efficiency by
-                        ~20% using <Highlight text={"debouncing"} /> (reducing redundant API calls
-                        by grouping user input requests into 500ms intervals).
-                     </div>
-                  </CustomPoint>
-                  <CustomPoint>
-                     <div>
-                        Developed an E-Cart Application from scratch, implementing product listing,
-                        search, cart management, shipping details, and a{" "}
-                        <Highlight text={"Razorpay payment gateway"} />.
-                     </div>
-                  </CustomPoint>
-                  <CustomPoint>
-                     <div>
-                        Created a <Highlight text={"responsive"} /> landing page for an educational
-                        website in close collaboration with UI/UX designers, ensuring pixel-perfect
-                        implementation.
-                     </div>
-                  </CustomPoint>
-                  <CustomPoint>
-                     <div>
-                        Designed an animated company landing page using{" "}
-                        <Highlight text={"Framer Motion"} /> , delivering a highly interactive and
-                        engaging user experience.
-                     </div>
-                  </CustomPoint>
-               </div>
+  const items = [
+    {
+      dot: (
+        <div className=" w-[15px] h-[15px] rounded-full bg-gray-200 flex-shrink-0" />
+      ),
+      children: (
+        <div className=" md:pr-[1rem]">
+          <div className=" flex justify-between items-center">
+            <div className=" text-themeOrange text-[1.1rem]">
+              Frontend Developer Intern{" "}
+              <span className=" text-[0.9rem] text-gray-300">
+                @Tikanga pvt ltd, Mangalore
+              </span>
             </div>
-         ),
-      },
-      {
-         dot: (
-            <div className=" w-[15px] h-[15px] rounded-full bg-gray-200 flex justify-center items-center flex-shrink-0">
-               <div className=" w-[15px] h-[15px] bg-themeOrange rounded-full animate-ping"></div>
+            <div className=" text-gray-300 mt-1 md:mt-0 text-[0.7rem] md:text-[1rem]">
+              Aug 2024 - Nov 2024
             </div>
-         ),
-         children: (
-            <div className=" md:pr-[1rem]">
-               <div className=" flex justify-between items-center">
-                  <div className=" text-themeOrange text-[1.1rem]">
-                     SDE{" "}
-                     <span className=" text-[0.9rem] text-gray-300">
-                        @Tikanga pvt ltd, Mangalore
-                     </span>
-                  </div>
-                  <div className=" text-gray-300 mt-1 md:mt-0 text-[0.7rem] md:text-[1rem]">Dec 2024 - Present</div>
-               </div>
-               <div className=" flex flex-col gap-[10px] mt-3">
-                  <CustomPoint>
-                     <div>
-                        Directly collaborated with clients to understand project requirements and
-                        deliver optimized solutions.
-                     </div>
-                  </CustomPoint>
-                  <CustomPoint>
-                     <div>
-                        Collaborated in frontend development for a Debate Management Application,
-                        taking full responsibility for the User Dashboard.
-                     </div>
-                  </CustomPoint>
-                  <CustomPoint>
-                     <div>
-                        Used Next.js, Tailwind CSS, and Ant Design to implement a fully responsive
-                        and scalable UI.
-                     </div>
-                  </CustomPoint>
-                  <CustomPoint>
-                     <div>
-                        Developed a seamless and efficient frontend experience, ensuring high
-                        performance and user satisfaction
-                     </div>
-                  </CustomPoint>
-               </div>
+          </div>
+          <div className=" flex flex-col gap-[10px] mt-3">
+            <CustomPoint>
+              <div>
+                Developed multiple web applications using{" "}
+                <Highlight text={"Next.js, TypeScript, React.js"} /> and{" "}
+                <Highlight text={"Ant Design"} />
+              </div>
+            </CustomPoint>
+            <CustomPoint>
+              <div>
+                Worked on a payment-related web application with 2000+ users,
+                integrating APIs, optimizing performance, and improving overall
+                website efficiency by ~20% using{" "}
+                <Highlight text={"debouncing"} /> (reducing redundant API calls
+                by grouping user input requests into 500ms intervals).
+              </div>
+            </CustomPoint>
+            <CustomPoint>
+              <div>
+                Developed an E-Cart Application from scratch, implementing
+                product listing, search, cart management, shipping details, and
+                a <Highlight text={"Razorpay payment gateway"} />.
+              </div>
+            </CustomPoint>
+            <CustomPoint>
+              <div>
+                Created a <Highlight text={"responsive"} /> landing page for an
+                educational website in close collaboration with UI/UX designers,
+                ensuring pixel-perfect implementation.
+              </div>
+            </CustomPoint>
+            <CustomPoint>
+              <div>
+                Designed an animated company landing page using{" "}
+                <Highlight text={"Framer Motion"} /> , delivering a highly
+                interactive and engaging user experience.
+              </div>
+            </CustomPoint>
+          </div>
+        </div>
+      ),
+    },
+    {
+      dot: (
+        <div className=" w-[15px] h-[15px] rounded-full bg-gray-200 flex justify-center items-center flex-shrink-0">
+          <div className=" w-[15px] h-[15px] bg-themeOrange rounded-full animate-ping"></div>
+        </div>
+      ),
+      children: (
+        <div className=" md:pr-[1rem]">
+          <div className=" flex justify-between items-center">
+            <div className=" text-themeOrange text-[1.1rem]">
+              SDE{" "}
+              <span className=" text-[0.9rem] text-gray-300">
+                @Tikanga pvt ltd, Mangalore
+              </span>
             </div>
-         ),
-      },
-   ];
-   return (
-      <div className=" lg:max-w-[78rem] mx-auto w-full min-h-[30rem] py-[10rem] pb-[3rem] border- flex flex-col gap-[3rem] md:gap-0 md:flex-row justify-cente items-centerr md:justify-between md:items-start">
-         <div className=" border- w-full text-center md:text-left lg:w-[90%] ">
-            <div className=" text-[1.4rem] text-gray-300 font-semibold">WORK EXPERIENCE</div>
-            <div className=" text-[2.3rem] text-themeOrange font-[500]">The Road So Far</div>
-         </div>
-         <div className=" w-full border- px-[1rem] md:px-0 ">
-            <ConfigProvider
-               theme={{
-                  components: {
-                     Timeline: {
-                        dotBg: "transparent",
-                        tailColor: "white",
-                     },
-                  },
-               }}
-            >
-               <Timeline mode="left" items={items} reverse={true} />
-            </ConfigProvider>
-         </div>
+            <div className=" text-gray-300 mt-1 md:mt-0 text-[0.7rem] md:text-[1rem]">
+              Dec 2024 - Present
+            </div>
+          </div>
+          <div className=" flex flex-col gap-[10px] mt-3">
+            <CustomPoint>
+              <div>
+                Directly collaborated with clients to understand project
+                requirements and deliver optimized solutions.
+              </div>
+            </CustomPoint>
+            <CustomPoint>
+              <div>
+                Collaborated in frontend development for a Debate Management
+                Application, taking full responsibility for the User Dashboard.
+              </div>
+            </CustomPoint>
+            <CustomPoint>
+              <div>
+                Used Next.js, Tailwind CSS, and Ant Design to implement a fully
+                responsive and scalable UI.
+              </div>
+            </CustomPoint>
+            <CustomPoint>
+              <div>
+                Developed a seamless and efficient frontend experience, ensuring
+                high performance and user satisfaction
+              </div>
+            </CustomPoint>
+          </div>
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div className=" lg:max-w-[78rem] mx-auto w-full min-h-[30rem] py-[10rem] pb-[3rem] border- flex flex-col gap-[3rem] md:gap-0 md:flex-row justify-cente items-centerr md:justify-between md:items-start">
+      <div className=" border- w-full text-center md:text-left lg:w-[90%] ">
+        <div className=" text-[1.4rem] text-gray-300 font-semibold">
+          WORK EXPERIENCE
+        </div>
+        <div className=" text-[2.3rem] text-themeOrange font-[500]">
+          The Road So Far
+        </div>
       </div>
-   );
+      <div className=" w-full border- px-[1rem] md:px-0 ">
+        <ConfigProvider
+          theme={{
+            components: {
+              Timeline: {
+                dotBg: "transparent",
+                tailColor: "white",
+              },
+            },
+          }}
+        >
+          <Timeline mode="left" items={items} reverse={true} />
+        </ConfigProvider>
+      </div>
+    </div>
+  );
 }
 
 const CustomPoint = ({ children }: { children: React.ReactNode }) => {
-   return (
-      <div className=" flex justify-start items-start text-[#b9b9b9] max-w-[38rem] gap-1 text-[1rem] text-left">
-         <GoDotFill size={16} className=" flex-shrink-0 mt-2" />
-         {children}
-      </div>
-   );
+  return (
+    <div className=" flex justify-start items-start text-[#b9b9b9] max-w-[38rem] gap-1 text-[1rem] text-left">
+      <GoDotFill size={16} className=" flex-shrink-0 mt-2" />
+      {children}
+    </div>
+  );
 };
 
 const Highlight = ({ text }: { text: String }) => {
-   return <span className=" font-[500] text-[#e0e0e0]">{text}</span>;
+  return <span className=" font-[500] text-[#e0e0e0]">{text}</span>;
 };
--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -3,13 +3,23 @@ import React from "react";
 
 export default function Skills() {
   return (
-    <div className=" w-full min-h-[10rem] border max-w-[78rem] mx-auto mb-[16rem] flex justify-center items-start">
+    <div className=" w-full min-h-[10rem] border- max-w-[78rem] mx-auto mb-[16rem] flex flex-col justify-center items-center">
+      <div className=" border- w-full text-center md:text-left mt-16 md:mt-0  ">
+        <div className=" text-[1.4rem] text-gray-300 font-semibold uppercase">
+          Skills
+        </div>
+        <div className=" text-[2.3rem] text-themeOrange font-[500]">
+          Weapons in My Arsenal
+        </div>
+      </div>
       {/* Tree  */}
-      <div className=" w-fit h-full flex flex-col items-center relative gap-[4rem]">
+      <div className=" w-fit h-full flex flex-col items-center relative gap-[4rem] scale-[0.6] md:scale-[1] lg:mt-16">
         {/* Main Node  */}
         <div className=" border-2 w-fit border-gray-200 text-gray-100 px-8 py-1 relative">
           <span>Full Stack</span>
-          <div className=" w-[16rem] h-8 border-r-2 border-t-2 border-l-2 border-gray-300 absolute bottom-[-4rem] left-[-4rem]">
+
+          {/* Connector  */}
+          <div className=" w-[25rem] h-8 border-r-2 border-t-2 border-l-2 border-gray-300 absolute bottom-[-4rem] left-[-8rem]">
             <div className="w-full absolute left-0 h-full flex justify-center items-center top-[-2rem]">
               <div className=" border-[1px] h-full border-gray-200"></div>
             </div>
@@ -41,8 +51,11 @@ export default function Skills() {
 
           {/* Backend  */}
           <div className=" w-fit ">
-            <div className=" w-fit border-2 border-gray-200 text-gray-100 px-8 py-1 ">
-              Backend
+            <div className=" w-fit border-2 border-gray-200 text-gray-100 px-8 py-1 relative ">
+              <span>Backend</span>
+              <div className="w-full absolute  left-[-9.5px] md:left-[8.5px] h-full flex justify-center items-center top-[-34px]">
+                <div className=" border-[1px] h-full border-gray-200"></div>
+              </div>
             </div>
             <ul className=" text-white list-disc mt-3 ml-5 ">
               <li>Node JS</li>
@@ -51,6 +64,21 @@ export default function Skills() {
               <li>JWT Authentication</li>
               <li>Middlewares</li>
               <li>Bycrypt</li>
+            </ul>
+          </div>
+
+          {/* Tools  */}
+          <div className=" w-fit ">
+            <div className=" w-fit border-2 border-gray-200 text-gray-100 px-8 py-1 ">
+              Tools
+            </div>
+            <ul className=" text-white list-disc mt-3 ml-5 ">
+              <li>Git</li>
+              <li>GitHub</li>
+              <li>Vercel</li>
+              <li>Netlify</li>
+              <li>Render</li>
+              <li>Postman</li>
             </ul>
           </div>
         </div>
