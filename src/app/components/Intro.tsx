465638da6fa7fcb9f32d411df0c298877c47d960
ShreeramNew <Shreerambca1@gmail.com>
2026-07-12 10:22:31 +0530

    updated new resume

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -11,7 +11,7 @@ export default function Intro() {
 
   const handleResume = () => {
     const a = document.createElement("a");
-    a.href = "https://drive.google.com/file/d/1MY3QXdc8wpC_2YOTZ7pCagRliIQsJT1L/view?usp=sharing";
+    a.href = "https://drive.google.com/file/d/12RK_Z2XjVtKP7wS0kGsZlOOZeUJlqAym/view?usp=sharing";
     a.download = "Resume.pdf";
     a.target = "_blank";
     a.click();
